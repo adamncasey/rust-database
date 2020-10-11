b44bc9000bd16b7986da7c27cc2360b905145c17
James Peach <jpeach6@bloomberg.net>
2020-10-11 19:04:55 +0100

    setup

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,5 @@
-mod database;
-mod query;
-
-#[cfg(test)]
-mod test;
-
-use std::io::{self, BufRead};
-
-fn repl(_input: &str) -> String {
-    "Not implemented".to_owned()
-}
+mod tuple;
 
 fn main() {
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        let input_string = line.unwrap();
-        println!("Input: {}", input_string);
-
-        repl(&input_string);
-    }
+    println!("Hello world!");
 }
