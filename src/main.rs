0de15f9f237d3dffa8d78cc9fd762528370fdf84
James Peach <JamesWP@users.noreply.github.com>
2020-11-17 11:39:18 +0000

    Switch parsers (#1)
    * switch to sqlparser for parsing
    
    * run build ci on pr
    
    Co-authored-by: James Peach <jpeach6@bloomberg.net>

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,30 @@ fn main() {
     println!("Hello, world!");
 
     let queries = [
-        "CREATE TABLE persons (personid, name, nickname, favourite_takeaway)",
-        "INSERT INTO persons VALUES (one, Adam-Casey, Adam, Jahangir)",
-        "INSERT INTO persons VALUES (two, Karl-Sykes, Kalleboballefofallemodalle, Maisha)",
+        "CREATE TABLE persons (personid varchar, name varchar, nickname varchar, favourite_takeaway varchar)",
+        "INSERT INTO persons VALUES ('one', 'Adam-Casey', 'Adam', 'Jahangir')",
+        "INSERT INTO persons VALUES ('two', 'Karl-Sykes', 'Kalleboballefofallemodalle', 'Maisha')",
         "SELECT * FROM persons",
     ];
 
     let mut db = Database::new();
 
     for query in &queries {
-        let plan: Plan = planner::plan(query);
+        let plan = planner::plan(query);
 
-        println!("Query {:?}", query);
-        println!("Plan {:?}", &plan);
+        if plan.is_err() {
+            println!("Plan error {:?}", plan.unwrap_err());
+            continue;
+        }
+
+        let plan = plan.unwrap();
+
+        println!("Query {:#?}", query);
+        println!("Plan {:#?}", &plan);
 
         let result: Result<DbRows, &'static str> = executor::execute(&plan, &mut db);
 
-        println!("Database {:?}", db);
+        println!("Database {:#?}", db);
 
         if let Ok(mut rows) = result {
             println!("Success");
@@ -34,7 +41,7 @@ fn main() {
                 println!("Row: {:?}", row);
             }
         } else {
-            println!("executor failed: {:?}", result);
+            println!("executor failed: {:#?}", result);
         }
     }
 }
